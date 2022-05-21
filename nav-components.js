d6ba50d52867b3d7a3de94004c718180e1c007f5
ramusayana <ramusayana@gmail.com>
2022-05-21 10:47:41 +0100

    Add files via upload

---
+++ nav-components.js
@@ -0,0 +1,13 @@
+
+
+class navComponent {
+  get linksNavMenu() {
+    return $$("#site-navigation-wrap.clr");
+  }
+
+  get firstNavMenuList() {
+    return $(("#site-navigation-wrap.clr");
+  }
+}
+
+export default new navComponent();
